cd9647130297855a8d3e4c7f57b30643373c539e
Saumya Kanoria <svk@humbleschool.in>
2021-12-13 12:12:14 +0530

    Adds eslint and prettier

--- index.js
+++ index.js
@@ -1,59 +1,71 @@
-require('dotenv').config()
-const path = require('path')
-const routes = require('./src/routes')
+require("dotenv").config();
 
-const lti = require('ltijs').Provider
-const Database = require('ltijs-sequelize')
+const path = require("path");
+const routes = require("./src/routes");
 
-const db = new Database(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,
+const lti = require("ltijs").Provider;
+const Database = require("ltijs-sequelize");
+
+const db = new Database(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false
-  })
+    dialect: "postgres",
+    logging: false,
+  }
+);
 
 // Setup
-lti.setup(process.env.LTI_KEY,
+lti.setup(
+  process.env.LTI_KEY,
   {
-    plugin: db // Passing db object to plugin field
+    plugin: db, // Passing db object to plugin field
   },
   {
-    staticPath: path.join(__dirname, './public'), // Path to static files
+    staticPath: path.join(__dirname, "./public"), // Path to static files
     cookies: {
       secure: true, // Set secure to true if the testing platform is in a different domain and https is being used
-      sameSite: 'None' // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
+      sameSite: "None", // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
     },
-    devMode: false // Set DevMode to true if the testing platform is in a different domain and https is not being used
-  })
+    devMode: false, // Set DevMode to true if the testing platform is in a different domain and https is not being used
+  }
+);
 
 // When receiving successful LTI launch redirects to app
 lti.onConnect(async (token, req, res) => {
-  return res.sendFile(path.join(__dirname, './public/index.html'))
-})
+  return res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 // When receiving deep linking request redirects to deep screen
 lti.onDeepLinking(async (token, req, res) => {
-  return lti.redirect(res, '/deeplink', { newResource: true })
-})
+  return lti.redirect(res, "/deeplink", { newResource: true });
+});
 
 // Setting up routes
-lti.app.use(routes)
+lti.app.use(routes);
 
 // Setup function
 const setup = async () => {
-  await lti.deploy({ port: process.env.PORT })
+  await lti.deploy({ port: process.env.PORT });
 
   /**
    * Register platform
    */
   await lti.registerPlatform({
-    url: 'https://humbleschool.moodlecloud.com',
-    name: 'HumbleSchool Moodle',
-    clientId: 't8NTZY9X8xAsKzl',
-    authenticationEndpoint: 'https://humbleschool.moodlecloud.com/mod/lti/auth.php',
-    accesstokenEndpoint: 'https://humbleschool.moodlecloud.com/mod/lti/token.php',
-    authConfig: { method: 'JWK_SET', key: 'https://humbleschool.moodlecloud.com/mod/lti/certs.php' }
-  })
-}
+    url: "https://humbleschool.moodlecloud.com",
+    name: "HumbleSchool Moodle",
+    clientId: "t8NTZY9X8xAsKzl",
+    authenticationEndpoint:
+      "https://humbleschool.moodlecloud.com/mod/lti/auth.php",
+    accesstokenEndpoint:
+      "https://humbleschool.moodlecloud.com/mod/lti/token.php",
+    authConfig: {
+      method: "JWK_SET",
+      key: "https://humbleschool.moodlecloud.com/mod/lti/certs.php",
+    },
+  });
+};
 
-setup()
+setup();
